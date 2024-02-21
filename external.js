c50884f12c5fb96b23184a6f34a8cda8a7828607
TobisMa <tobias-ma@gmx.net>
2024-02-21 21:25:45 +0100

    import/export data and print

--- external.js
+++ external.js
@@ -1,4 +1,5 @@
 const KEY_LAST_FILENAME = "last_filename_export";
+const CSV_DELIMITER = ";";
 
 
 function exportData(e) {
@@ -8,8 +9,10 @@ function exportData(e) {
         return;
     }
     const content = JSON.stringify({
-        "students": globalStudents,
-        "room": roomStudents
+        students: globalStudents,
+        room: roomStudents,
+        width: width,
+        height: height,
     });
     saveToStorage(KEY_LAST_FILENAME, filename)
     let f = new File([content], filename, {type: "text/json"});
@@ -26,7 +29,7 @@ function exportData(e) {
 }
 
 
-function importData(data) {
+function importData(data, filetyp) {
     while (studentListNode.firstElementChild !== studentListNode.lastElementChild) {
         studentListNode.removeChild(studentListNode.firstChild);
     }
@@ -34,10 +37,53 @@ function importData(data) {
     if (typeof data === "object") {
         globalStudents = data.students ?? [];
         roomStudents = data.room ?? {};
+        let w = data.width ?? 12;
+        let num = parseInt(w);
+        if (num === NaN) {
+            w = 12;
+        }
+        sliderHorizontal.value = w;
+        sliderHorizontal.dispatchEvent(new Event("change"));
+        
+        let h = data.width ?? 12;
+        num = parseInt(h);
+        if (num === NaN) {
+            h = 12;
+        }
+        sliderVertical.value = h;
+        sliderVertical.dispatchEvent(new Event("change"));
 
         globalStudents.forEach(addStudentHTML);  // make room removes again
         saveToStorage(KEY_STUDENT, globalStudents);
 
+        roomTables.innerHTML = "";
+        makeRoom();  // stores
+        saveToStorage(KEY_ROOM, roomStudents);
+    }
+    else if (typeof data === "string" && filetyp === "csv") {
+        let lines = data.split("\n").slice(1);
+        console.log(lines);
+        console.log("Found csv");
+        let students = lines
+            .filter(line => line)
+            .map(line => line.split(CSV_DELIMITER))  // convert lines to values
+            .map(values => [values[1], values[2]])       // convert values to names
+            .map(names => names.map(name => name.trim("\"")))  // remove possible "
+            .map(names => names.join(", "));        // make it to one string
+        
+        console.log(students);
+        
+        let answer = confirm("Import following students (and so on if there are more)?:\n" + students.slice(0, 5).join("\n") + "\n...");
+        if (!answer) {
+            alert("Canceled");
+            return;
+        }
+
+        globalStudents = students;
+        globalStudents.forEach(addStudentHTML);
+        saveToStorage(KEY_STUDENT, globalStudents);
+
+        roomStudents = {};
         roomTables.innerHTML = "";
         makeRoom();  // stores
         saveToStorage(KEY_ROOM, roomStudents);
@@ -50,7 +96,7 @@ function importData(data) {
             return;
         }
 
-        globalStudents = lines;
+        globalStudents = lines.filter(line => line);
         globalStudents.forEach(addStudentHTML);
         saveToStorage(KEY_STUDENT, globalStudents);
 
@@ -70,14 +116,28 @@ function importDataHandler(e) {
         for (var i = 0; i < files.length; i++){
             let read = new FileReader();
             read.readAsBinaryString(files[i]);
-            read.onloadend = (e) => { readFileImport(read); };
+            read.onloadend = (e) => { readFileImport(read, getFileExtension(files[i].name));};
             break;
         }
     }
     input.click();
 }
 
-function readFileImport(reader) {
+function getFileExtension(filename) {
+    if (filename.includes(".")) {
+        let splitted = filename.split(".");
+        ext = splitted[splitted.length - 1].toLowerCase();
+        return ext;
+    }
+    return undefined;
+}
+
+function readFileImport(reader, ext) {
+    if (reader.result === null) {
+        console.log("Probably folder dropped");
+        return;
+    }
+    console.log(reader);
     let jsonData = undefined;
     try {
         jsonData = JSON.parse(reader.result);
@@ -86,9 +146,9 @@ function readFileImport(reader) {
         // console.log(e);
     }
     if (jsonData) {
-        importData(JSON.parse(reader.result));
+        importData(JSON.parse(reader.result), ext);
     } else {
-        importData(reader.result);
+        importData(reader.result, ext);
     }
 }
 
@@ -118,7 +178,7 @@ function handleDrop(e) {
     let file = dt.files[0];
     let fileReader = new FileReader();
     fileReader.readAsBinaryString(file);
-    fileReader.onloadend = (e) => { readFileImport(fileReader); };
+    fileReader.onloadend = (e) => { readFileImport(fileReader, getFileExtension(file.name)); };
 }
 
 window.addEventListener("DOMContentLoaded", (e) => {
